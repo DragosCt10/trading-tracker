18753f1741e5f0bc0f1a4df9868b1fe4de15a251
dragosct10 <dragos@creative-tim.com>
2025-05-21 15:56:02 +0300

    Update dashboard.ts

--- src/types/dashboard.ts
+++ src/types/dashboard.ts
@@ -26,3 +26,90 @@ export interface MacroStats {
   consistencyScoreWithBE: number;  // including BE
   sharpeWithBE: number;            // including BE
 }
+
+export interface BaseStats {
+  total: number;
+  wins: number;
+  losses: number;
+  winRate: number;
+  winRateWithBE: number;
+  beWins: number;
+  beLosses: number;
+}
+
+export interface LiquidityStats extends BaseStats {
+  liquidity: string;
+}
+
+export interface SetupStats extends BaseStats {
+  setup: string;
+}
+
+export interface DirectionStats extends BaseStats {
+  direction: string;
+}
+export interface DayStats extends BaseStats {
+  day: string;
+}
+
+export interface MarketStats extends BaseStats {
+  market: string;
+}
+
+export interface EvaluationStats extends BaseStats {
+  grade: string;
+}
+
+export interface TradeTypeStats extends BaseStats {
+  tradeType: string;
+}
+
+export interface NewsStats extends BaseStats {
+  news: string;
+}
+
+export interface MssStats extends BaseStats {
+  mss: string;
+}
+
+export interface SLSizeStats {
+  market: string;
+  averageSlSize: number;
+}
+
+export interface LocalHLStats {
+  [key: string]: {
+    wins: number;
+    losses: number;
+    winRate: number;
+    winsWithBE: number;
+    lossesWithBE: number;
+    winRateWithBE: number;
+  }
+}
+
+export interface IntervalStats {
+  label: string;
+  wins: number;
+  losses: number;
+  beWins: number;
+  beLosses: number;
+  winRate: number;
+  winRateWithBE: number;
+}
+
+export interface Stats {
+  totalTrades: number;
+  totalWins: number;
+  totalLosses: number;
+  winRate: number;
+  totalProfit: number;
+  averageProfit: number;
+  intervalStats: Record<string, IntervalStats>;
+  maxDrawdown: number;
+  averagePnLPercentage: number;
+  evaluationStats: EvaluationStats[];
+  winRateWithBE: number;
+  beWins: number;
+  beLosses: number;
+}
