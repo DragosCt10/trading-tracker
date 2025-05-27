c1ec2e56a34e8cda2cf33efd57bdc17e54957898
dragosct10 <dragos@creative-tim.com>
2025-05-27 12:11:06 +0300

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -952,7 +952,7 @@ export default function Dashboard() {
                 className="aria-disabled:cursor-not-allowed w-40 appearance-none outline-none cursor-pointer focus:outline-none text-stone-800 placeholder:text-stone-600/60 ring-transparent border border-stone-200 transition-all ease-in disabled:opacity-50 disabled:pointer-events-none select-none text-sm py-2 px-2.5 ring shadow-sm bg-white rounded-lg duration-100 hover:border-stone-300 hover:ring-none focus:border-stone-400 focus:ring-none peer"
               >
                 <option value="all">All Markets</option>
-                {Array.from(new Set(filteredTrades.map(trade => trade.market))).map(market => (
+                {Array.from(new Set(allTrades.map(trade => trade.market))).map(market => (
                   <option key={market} value={market}>{market}</option>
                 ))}
               </select>
