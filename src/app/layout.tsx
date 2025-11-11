459ca8b01a7c1d9eb2be30378af0277ac12504dc
dragosct10 <dragos@creative-tim.com>
2025-11-11 14:48:30 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { TradingModeProvider } from '@/context/TradingModeContext';
 import { LoadingProvider } from '@/context/LoadingContext';
 import Footer from '@/components/shared/Footer';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import QueryProvider from '@/context/QueryContext';
 
 
@@ -25,16 +23,13 @@ export default function RootLayout({
       <body className={inter.className}>
         <QueryProvider>
           <LoadingProvider>
-            <TradingModeProvider>
               <div className="min-h-screen bg-gray-50">
                 <main className="max-w-(--breakpoint-xl) p-4 md:px-0 mx-auto pt-32">
                   {children}
                 </main>
                 <Footer />
               </div>
-            </TradingModeProvider>
           </LoadingProvider>
-          <ReactQueryDevtools initialIsOpen={false} />
         </QueryProvider>
       </body>
     </html>
