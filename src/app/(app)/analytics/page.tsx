309079e58cd20f6fb274f5b288865b2256c47ce0
dragosct10 <dragos@creative-tim.com>
2026-02-14 12:34:41 +0200

    Update page.tsx

--- src/app/(app)/analytics/page.tsx
+++ src/app/(app)/analytics/page.tsx
@@ -1,8 +1,11 @@
-import AnalyticsClient from "./AnalyticsClient";
+import { getCachedUserSession } from '@/lib/server/trades';
+import { redirect } from 'next/navigation';
+import AnalyticsData from './AnalyticsData';
 
+export const dynamic = 'force-dynamic';
 
-export default function AnalyticsPage() {
-  return (
-    <AnalyticsClient />
-  );
+export default async function AnalyticsPage() {
+  const { user } = await getCachedUserSession();
+  if (!user) redirect('/login');
+  return <AnalyticsData user={user} />;
 }
