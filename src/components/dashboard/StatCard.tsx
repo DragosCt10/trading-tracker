c3b2bc925d69424c5c17b919bd6cffc8cf02aacc
dragosct10 <dragos@creative-tim.com>
2026-02-14 17:45:11 +0200

    Update StatCard.tsx

--- src/components/dashboard/StatCard.tsx
+++ src/components/dashboard/StatCard.tsx
@@ -24,7 +24,7 @@ export const StatCard: React.FC<StatCardProps> = ({
   return (
     <Card
       className={cn(
-        'relative overflow-hidden border-slate-200/60 dark:border-slate-700/50 bg-slate-50/70 dark:from-slate-900 dark:via-slate-900/95 dark:to-slate-900 shadow-lg shadow-slate-200/50 dark:shadow-none backdrop-blur-sm flex-1 flex flex-col',
+        'relative overflow-hidden border-slate-200/60 dark:border-slate-700/50 bg-gradient-to-br bg-slate-50/70 dark:bg-gradient-to-br dark:from-slate-900 dark:via-slate-900/95 dark:to-slate-900 shadow-lg shadow-slate-200/50 dark:shadow-none backdrop-blur-sm flex-1 flex flex-col',
         className
       )}
     >
@@ -97,7 +97,7 @@ export const StatCard: React.FC<StatCardProps> = ({
                   )}
                 </div>
             </div>
-            <div className="flex flex-col items-end shrink-0">{value}</div>
+            <div className="flex flex-col items-end shrink-0">{value}</div> 
           </div>
         )}
       </div>
