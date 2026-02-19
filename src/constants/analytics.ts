22edc3477c725e5c7342c98604a8bd2e575f1c50
dragosct10 <dragos@creative-tim.com>
2026-02-19 18:42:21 +0200

    Create analytics.ts

---
+++ src/constants/analytics.ts
@@ -0,0 +1,6 @@
+export const TIME_INTERVALS = [
+  { label: '< 10 a.m', start: '00:00', end: '09:59' },
+  { label: '10 a.m - 12 p.m', start: '10:00', end: '11:59' },
+  { label: '12 p.m - 16 p.m', start: '12:00', end: '16:59' },
+  { label: '17 p.m - 21 p.m', start: '17:00', end: '20:59' },
+] as const;
