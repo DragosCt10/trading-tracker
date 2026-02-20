1c17ab306f61125403e2cdaaa4e097120ee42c20
dragosct10 <dragos@creative-tim.com>
2026-02-20 19:18:22 +0200

    Delete filterExecutedTrades.ts

--- src/utils/filterExecutedTrades.ts
+++
@@ -1,8 +0,0 @@
-import { Trade } from '@/types/trade';
-
-/**
- * Filters out trades that have not been executed (executed: false)
- */
-export function filterExecutedTrades(trades: Trade[]): Trade[] {
-  return trades.filter(trade => trade.executed !== false);
-}
