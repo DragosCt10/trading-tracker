e271f4d4c361f2469befa0300a0cae34b10c906c
dragosct10 <dragos@creative-tim.com>
2026-02-21 14:12:18 +0200

    Update useDashboardData.ts

--- src/hooks/useDashboardData.ts
+++ src/hooks/useDashboardData.ts
@@ -44,14 +44,7 @@ import {
 import { calculateEvaluationStats } from '@/utils/calculateEvaluationStats';
 import { calculateTradeQualityIndex } from '@/utils/calculateTradeQualityIndex';
 import { calculateRRStats } from '@/utils/calculateRMultiple';
-
-
-const TIME_INTERVALS = [
-  { label: '< 10 a.m', start: '00:00', end: '09:59' },
-  { label: '10 a.m - 12 p.m', start: '10:00', end: '11:59' },
-  { label: '12 p.m - 16 p.m', start: '12:00', end: '16:59' },
-  { label: '17 p.m - 21 p.m', start: '17:00', end: '20:59' },
-] as const;
+import { TIME_INTERVALS } from '@/constants/analytics';
 
 export function useDashboardData({
   session,
