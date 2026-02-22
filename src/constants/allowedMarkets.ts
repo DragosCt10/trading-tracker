6d4b469ea52e4181faa64b801b021bfd1acca0fc
dragosct10 <dragos@creative-tim.com>
2026-02-22 18:06:24 +0200

    Update allowedMarkets.ts

--- src/constants/allowedMarkets.ts
+++ src/constants/allowedMarkets.ts
@@ -17,7 +17,7 @@ export const ALLOWED_MARKETS: string[] = [
   'NZD/CAD', 'NZD/CHF', 'NZD/JPY', 'NZD/USD',
   'USD/CAD', 'USD/CHF', 'USD/JPY', 'USD/NOK', 'USD/SEK', 'USD/SGD', 'USD/TRY', 'USD/ZAR',
   // Indices
-  'AU200', 'DE30', 'DE40', 'DE30EU', 'EU50', 'EUSTX50', 'FR40', 'HK50', 'IT40', 'JP225', 'NAS100', 'NASDAQ', 'NDX',
+  'AU200', 'DE30', 'DE40', 'DE30EU', 'EU50', 'EUSTX50', 'FR40', 'GER30', 'HK50', 'IT40', 'JP225', 'NAS100', 'NASDAQ', 'NDX',
   'SPX', 'SPX500', 'UK100', 'US30', 'US100', 'US500', 'USTEC', 'VIX', 'DAX', 'GER40', 'UKOIL', 'USOIL', 'WS30',
   // Commodities
   'BRENT', 'COPPER', 'GOLD', 'NATGAS', 'SILVER', 'WTI', 'XAGUSD', 'XAUUSD', 'XAU/USD', 'XAG/USD', 'WTI/USD', 'BCO/USD',
@@ -32,7 +32,7 @@ export const ALLOWED_MARKETS: string[] = [
   'COTTON', 'COFFEE', 'SUGAR', 'CORN', 'WHEAT', 'SOYBEAN', 'NGAS', 'PLATINUM', 'PALLADIUM', 'XPTUSD', 'XPDUSD',
   // Futures – indices (CME etc.)
   'ES', 'NQ', 'YM', 'RTY', 'MES', 'MNQ', 'MYM', 'M2K', 'EMD', 'NQD', 'ES1', 'NQ1', 'YM1', 'RTY1',
-  'SPX500', 'USTEC', 'US30', 'US500', 'US2000', 'FDAX', 'FESX', 'FGBL', 'FGBM', 'FGBX', 'STOXX50',
+  'FDAX', 'FESX', 'FGBL', 'FGBM', 'FGBX',
   // Futures – commodities (CME, ICE)
   'CL', 'BZ', 'NG', 'GC', 'SI', 'MGC', 'SIL', 'HG', 'ZC', 'ZW', 'ZS', 'ZM', 'ZL', 'LE', 'GF', 'HE',
   'RB', 'HO', 'QM', 'QM1', 'CL1', 'NG1', 'GC1', 'SI1', 'ZC1', 'ZW1', 'ZS1',
@@ -40,8 +40,6 @@ export const ALLOWED_MARKETS: string[] = [
   '6E', '6J', '6B', '6A', '6C', '6N', '6S', 'M6E', 'M6J', 'M6B', 'E7', 'J7', 'EC', 'JY', 'BP', 'AD', 'CD', 'MP', 'SF',
   // Futures – bonds / rates (ZB=30Y, ZN=10Y, ZF=5Y, ZT=2Y, GE=Eurodollar)
   'ZB', 'ZN', 'ZF', 'ZT', 'UB', 'GE', 'FF', 'SOFR', 'TY', 'US', 'FV', 'TU',
-  // Duplicates / common aliases
-  'GER30', 'NAS100', 'USOIL', 'UKOIL', 'XAUUSD', 'GOLD', 'SILVER', 'XAGUSD', 'BRENT', 'WTI',
 ].filter((v, i, a) => a.indexOf(v) === i).sort((a, b) => a.localeCompare(b));
 
 const ALLOWED_MARKETS_UPPER = new Set(ALLOWED_MARKETS.map((m) => m.toUpperCase()));
