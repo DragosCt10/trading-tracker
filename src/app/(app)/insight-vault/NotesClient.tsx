e7cf107d5de6add339a7a7c10ca0e92f266b2212
dragosct10 <dragos@creative-tim.com>
2026-02-25 14:34:03 +0200

    Update NotesClient.tsx

--- src/app/(app)/insight-vault/NotesClient.tsx
+++ src/app/(app)/insight-vault/NotesClient.tsx
@@ -267,6 +267,7 @@ export default function NotesClient({
                 key={note.id}
                 note={note}
                 onClick={() => openModal(note)}
+                userId={userId}
               />
             ))}
             
