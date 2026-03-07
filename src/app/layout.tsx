6420900904b338c7ac707531ab82c2277b4cba7d
dragosct10 <dragos@creative-tim.com>
2026-03-07 22:38:43 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { LoadingProvider } from '@/context/LoadingContext';
-import Footer from '@/components/shared/Footer';
 import QueryProvider from '@/context/QueryContext';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -25,7 +24,6 @@ export default function RootLayout({
             <main className="max-w-(--breakpoint-xl) p-4 md:px-0 mx-auto pt-32">
               {children}
             </main>
-            <Footer />
           </LoadingProvider>
         </QueryProvider>
       </body>
