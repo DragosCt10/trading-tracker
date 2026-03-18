285306d79abd761c019104d8c7ce94738a51ccc4
dragosct10 <dragos@creative-tim.com>
2026-03-18 16:31:59 +0200

    Update page.tsx

--- src/app/(app)/(inside-strategy)/strategy/[strategy]/daily-journal/page.tsx
+++ src/app/(app)/(inside-strategy)/strategy/[strategy]/daily-journal/page.tsx
@@ -1,5 +1,4 @@
 import { getCachedUserSession } from '@/lib/server/session';
-import { resolveSubscription } from '@/lib/server/subscription';
 import { redirect } from 'next/navigation';
 import DailyJournalData from './DailyJournalData';
 
@@ -12,11 +11,6 @@ interface PageProps {
 export default async function DailyJournalPage({ params }: PageProps) {
   const { user } = await getCachedUserSession();
   if (!user) redirect('/login');
-
-  const subscription = await resolveSubscription(user.id);
-  if (!subscription.definition.features.dailyJournal) {
-    redirect('/billing?feature=daily-journal');
-  }
 
   const resolvedParams = await params;
   const strategySlug = decodeURIComponent(resolvedParams.strategy);
