3038fdd21703118430319c54b735a4512d0d3849
dragosct10 <dragos@creative-tim.com>
2026-03-26 12:20:26 +0200

    Update page.tsx

--- src/app/(app)/stats/page.tsx
+++ src/app/(app)/stats/page.tsx
@@ -24,6 +24,7 @@ export default async function StrategiesPage() {
 
   return (
     <StrategiesClient
+      initialUserId={user.id}
       initialActiveAccount={initialActiveAccount}
       initialMode={initialMode}
       initialStrategies={initialStrategies}
