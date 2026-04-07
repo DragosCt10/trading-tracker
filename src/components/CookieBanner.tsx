bfe455a5f25c734d4068c5aec0ead94c24a0fe71
dragosct10 <dragos@creative-tim.com>
2026-04-07 16:03:30 +0300

    Update CookieBanner.tsx

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -14,7 +14,7 @@ function updateGtagConsent(granted: boolean) {
   });
 
   // Some GTM tags listen for this custom event to trigger on consent update, so we need to dispatch it manually.
-  window.dataLayer?.push({ event: 'consent_update' });
+  // window.dataLayer?.push({ event: 'consent_update' });
 }
 
 export function CookieBanner() {
