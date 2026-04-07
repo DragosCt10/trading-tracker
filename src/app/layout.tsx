6391ea08dd6803ead471891479bc7996215fec08
dragosct10 <dragos@creative-tim.com>
2026-04-07 15:46:50 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,14 +77,18 @@ export default async function RootLayout({
             {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
+              window.gtag = gtag;
               gtag('consent', 'default', {
                 'ad_storage': 'denied',
+                'ad_user_data': 'denied',
+                'ad_personalization': 'denied',
                 'analytics_storage': 'denied',
-                'wait_for_update': 500
+                'wait_for_update': 3000
               });
             `}
           </Script>
         )}
+
         {/* Apply theme before first paint to avoid flash of default theme on refresh */}
         <script
           nonce={nonce}
